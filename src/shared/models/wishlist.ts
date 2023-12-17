70f2f5a79a8d242c18bec0daa6478174d8cca9db
Ashvin Santhosh <60149864+Vrindtime@users.noreply.github.com>
2023-12-18 00:49:46 +0530

    god help

--- src/shared/models/wishlist.ts
+++
@@ -1,6 +0,0 @@
-export class wishList{
-    constructor(public wishtext:string  , public isComplete : boolean=false){
-        //get the wishtext from the import file and the isComplete
-        //in the event the isComplete is not Provided it will take the default value of false
-    }
-}
